3fb5d52a4be0622d7a0ad2a83c3c0b2d699bfa82
saeed mirjalili <154972495+saeed-mirjalili@users.noreply.github.com>
2025-03-13 21:54:14 +0330

    Update OtpConf.php

--- src/Config/OtpConf.php
+++ src/Config/OtpConf.php
@@ -5,5 +5,5 @@ return
     'WhatsApp_Intance' => 'instancexxxxxx',
     'WhatsApp_Token' => 'xxxxxxxxxxx',
 
-    'TELEGRAM_BOT_TOKEN' => '7968333352:AAG0CJQKlAjQ3Bgqg5iCb3x7ErRvRwXaBY8'
+    'TELEGRAM_BOT_TOKEN' => 'xxxxxxxxxx'
 ];
